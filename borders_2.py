420eadda90714b59b798aa853ead1e519e539610
Szymyy <144834785+Szymyy@users.noreply.github.com>
2023-11-02 10:06:40 +0000

    rubbish

--- borders_2.py
+++ borders_2.py
@@ -1,4 +1,5 @@
 import pygame
+pygame.init()
 # imports the pygame library used for the project
 window = pygame.display.set_mode((500, 500))
 # sets the window size
@@ -6,22 +7,35 @@ window = pygame.display.set_mode((500, 500))
 class button():
   # takes in the list of the x and y size of the button and a list of 
   # the x and y coordinates of the button
-  def __init__(self, size, location):
+  # graphics will currently pass the RGB code for a colour of a button
+  def __init__(self, size, location, graphics, function):
     self.size = size
     self.location = location
+    self.graphics = graphics
+    self.function = function
 
-button_1 = button((50,50),(225,225))
+  def if_clicked(self):
+    self.funtion
+
+button_1 = button((50,50),(225,225), 0, 0)
+buttons = []
+buttons.append(button_1)
 # initalises an itnitial button object for testing
 
-
 def menu():
+  for event in pygame.event.get():
+    if event.type == pygame.MOUSEBUTTONDOWN:
+      for button in buttons:
+        
   background = pygame.image.load("Background test.png")
   title = pygame.image.load("Title test.png")
-  button = pygame.image.load("Button test.png")
   window.blit(background,(0,0))
   window.blit(title,(125,50))
-  window.blit(button,button_1.location)
+  
   pygame.display.update()
 
-menu()
+running = True
+while running:
+  menu()
+
 
