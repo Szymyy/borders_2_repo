22f1f5ec96c0a7b3af45282a6de46a413f3863e9
Szymyy <144834785+Szymyy@users.noreply.github.com>
2023-11-06 10:32:04 +0000

    button class finished so far, menu function added event detection, new function event_handler() made but not yet impleneted

--- borders_2.py
+++ borders_2.py
@@ -3,6 +3,7 @@ pygame.init()
 # imports the pygame library used for the project
 window = pygame.display.set_mode((500, 500))
 # sets the window size
+clock = pygame.time.Clock()
 
 class button():
   # takes in the list of the x and y size of the button and a list of 
@@ -14,7 +15,7 @@ class button():
     self.graphics = graphics
     self.function = function
 
-  def if_clicked(self):
+  def clicked(self):
     self.funtion
 
 button_1 = button((50,50),(225,225), 0, 0)
@@ -23,10 +24,7 @@ buttons.append(button_1)
 # initalises an itnitial button object for testing
 
 def menu():
-  for event in pygame.event.get():
-    if event.type == pygame.MOUSEBUTTONDOWN:
-      for button in buttons:
-        
+  menu_events = pygame.event.get()
   background = pygame.image.load("Background test.png")
   title = pygame.image.load("Title test.png")
   window.blit(background,(0,0))
@@ -34,8 +32,11 @@ def menu():
   
   pygame.display.update()
 
+def event_handler():
+  pass
+
 running = True
 while running:
   menu()
-
+  clock.tick(30) # limits the game to 30 fps
 
