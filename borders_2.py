2a471d469560629837f6a27d8e9d04918d1bdc2f
Szymyy <144834785+Szymyy@users.noreply.github.com>
2023-11-25 21:01:16 +0000

    Added document.py and document class

--- borders_2.py
+++ borders_2.py
@@ -1,6 +1,7 @@
 import pygame
 import button
 import graphics_handler
+import document
 
 
 pygame.init()
---
+++ document.py
@@ -0,0 +1,36 @@
+import random
+import pygame
+
+class document():
+  def __init__(self, size, location, graphics, surface):
+    self.size = size
+    self.location = location
+    self.graphics = graphics
+    self.surface = surface
+    self.valid = True
+
+  def draw(self):
+    pygame.draw.rect(self.surface, self.graphics, (self.location, self.size))
+
+  def randomise(self):
+    a = random.randint(0,10)
+    if a < 7:
+      self.valid = True
+      self.graphics = (0,255,0)
+
+    else:
+      self.valid = False
+      self.graphics = (255,0,0)
+
+  def accept(self):
+    if self.valid:
+      return True
+    else:
+      return False
+
+  def deny(self):
+    if self.valid:
+      return False
+    else:
+      return True
+  
